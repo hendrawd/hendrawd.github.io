690545ce7262e8c4c138c4fd4613e70ac802f7e5
Hendra Wijaya Djiono <hendrawd88@gmail.com>
2016-12-27 15:31:39 +0700

    Fix google maps and add contact us menu

--- js/main.js
+++ js/main.js
@@ -51,45 +51,45 @@ $(window).load(function() {
 // });
 
 /* Google map */
-// $(function () {
-//   "use strict";
-// 	var map = new GMaps({
-// 	el: "#map",
-// 	lat: 40.714353,
-// 	lng: -74.005973,
-//           zoom: 15,
-//           zoomControl : true,
-//           zoomControlOpt: {
-//             style : "BIG",
-//             position: "TOP_LEFT"
-//           },
-//           panControl : true,
-//           streetViewControl : false,
-//           mapTypeControl: false,
-//           overviewMapControl: false
-//       });
-//
-//       var styles = [
-//             {
-//               stylers: [
-//                 { hue: "#00ffe6" },
-//                 { saturation: -100 }
-//               ]
-//             }
-//       ];
-//
-//       map.addStyle({
-//             styledMapName:"Styled Map",
-//             styles: styles,
-//             mapTypeId: "map_style"
-//       });
-//
-//       map.setStyle("map_style");
-//
-//       map.addMarker({
-//         lat: 40.714353,
-//         lng: -74.005973,
-//         icon: "images/marker.png"
-//       });
-// });
+$(function () {
+  "use strict";
+	var map = new GMaps({
+	el: "#map",
+	lat: -6.2169131,
+	lng: 106.8121203,
+          zoom: 15,
+          zoomControl : true,
+          zoomControlOpt: {
+            style : "BIG",
+            position: "TOP_LEFT"
+          },
+          panControl : true,
+          streetViewControl : false,
+          mapTypeControl: false,
+          overviewMapControl: false
+      });
+
+      var styles = [
+            {
+              stylers: [
+                { hue: "#00ffe6" },
+                { saturation: -100 }
+              ]
+            }
+      ];
+
+      map.addStyle({
+            styledMapName:"Styled Map",
+            styles: styles,
+            mapTypeId: "map_style"
+      });
+
+      map.setStyle("map_style");
+
+      map.addMarker({
+      	lat: -6.2169131,
+	  	lng: 106.8121203,
+        icon: "images/marker.png"
+      });
+});
 
